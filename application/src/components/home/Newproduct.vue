6d626f2c6e379fcd8dde69b81e867e497a09d181
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2020-02-11 21:36:30 +0600

    Completed the new product sections

--- application/src/components/home/Newproduct.vue
+++ application/src/components/home/Newproduct.vue
@@ -11,22 +11,47 @@
           velit pariatur magnam fuga maiores
         </p>
         <v-row class="mt-5">
-          <v-col cols="12" md="3" sm="12" xs="12">
+          <v-col cols="12" md="3" sm="12" xs="12" v-for="todo in 4" :key="todo">
             <div class="newproducts">
-              <img
-                src="@/assets/img/7.jpg"
-                alt="Products"
-                class="newproducts__photo"
-              />
-              <h4 class="newproducts__header">hello</h4>
-              <p class="newproducts__catagory">hello</p>
+              <div class="newproducts__img">
+                <div class="newproducts__img-overlay"></div>
+                <img
+                  src="@/assets/img/7.jpg"
+                  alt="Products"
+                  class="newproducts__photo"
+                />
+                <div class="newproducts__img-content " align="center">
+                  <div class="my-2">
+                    <v-btn class="btn-cart" medium color="error" dark
+                      >Add to Cart</v-btn
+                    >
+                  </div>
+                  <div class="my-2">
+                    <v-btn medium color="primary" class="btn-cart" dark
+                      >Quick View</v-btn
+                    >
+                  </div>
+                </div>
+              </div>
+
+              <a href="#" class="newproducts__catagory mt-2">Organic Protein</a>
+              <a href="#" class="newproducts__header ">Organic Protein</a>
+
               <v-rating
+                class="newproducts__rating"
                 v-model="rating"
                 background-color="pink lighten-3"
                 color="pink"
-              ></v-rating>
-              <p class="newproducts__price">
-                BDT: 12
+                small
+                dense
+                half-increments
+                hover
+              >
+              </v-rating
+              ><span class="newproducts__rating-count">({{ rating }})</span>
+
+              <p class="newproducts__price mt-3">
+                <i class="fas fa-lira-sign"></i> 122
               </p>
             </div>
           </v-col>
@@ -37,7 +62,13 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      rating: 0
+    };
+  }
+};
 </script>
 
 <style></style>
