63559ab396a22c4fb6e107a8dc5518841e41ff8e
caffeines <sadat.talks@gmail.com>
2020-02-27 15:37:08 +0600

    saving

--- customer/index.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-
-const server = require('./app');
-const configServer = require('./config/server');
-const port = Number(process.env.PORT) || configServer.port;
-
-server.listen(port, () => {
-  console.log(chalk.bgGreen(`server listening on port ${port}...`));
-});
