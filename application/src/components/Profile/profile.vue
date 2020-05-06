65b9e356ac1d59007fdbe51dbb487ea662da6f19
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2020-05-06 23:40:12 +0600

    added profile picture and name section

--- application/src/components/Profile/profile.vue
+++ application/src/components/Profile/profile.vue
@@ -5,7 +5,20 @@
         <div class="profile-img">
           <img src="@/assets/img/profile.png" alt="Profile Image" class="profile-img__photo" />
         </div>
-        <div class="profile-info"></div>
+        <div class="profile-info">
+          <p class="profile-info__nametitle">
+            Name:
+            <span class="profile-info__name">John Doe</span>
+          </p>
+          <p class="profile-info__emailadd">
+            Email:
+            <span class="profile-info__email">something@example.com</span>
+          </p>
+          <p class="profile-info__phone">
+            Phone:
+            <span class="profile-info__phonenumber">880192929292</span>
+          </p>
+        </div>
       </div>
       <div class="profile-nav">
         <ul class="pnav-list">
