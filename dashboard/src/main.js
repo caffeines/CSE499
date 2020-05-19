672af1ea1dbbbf4e141791298acdc2847b37d77d
Muradkhan0161 <murad.khan@northsouth.edu>
2020-05-19 20:47:57 +0600

    drawer added

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -6,12 +6,13 @@ import store from './store';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: (h) => h(App),
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
 }).$mount('#app');
---
+++ dashboard/src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <dashboard-core-drawer />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DashboardIndex',
+
+  components: {
+    DashboardCoreDrawer: () => import('../components/Drawer/Drawer'),
+  },
+};
+</script>
